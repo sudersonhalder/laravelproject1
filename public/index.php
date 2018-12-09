df9c9d7a864497ba26799a2f54f6e3b0dab3d816
sudersonhalder <45104753+sudersonhalder@users.noreply.github.com>
2018-12-09 13:34:27 +0530

    sud8

--- public/index.php
+++ public/index.php
@@ -35,7 +35,7 @@
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+//-$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
